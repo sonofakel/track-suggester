bc3301532a4248166df7a4919610a55e102c9625
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-07 11:52:28 -0700

    ask user for first name and then add logic to have name with language specific response generated after user clicks submit.

--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,39 @@ $(function(){
     var websiteType= $("input:radio[name=website-type]").val();
     var whyProgram = $("input:radio[name=why-program]").val();
     var favToy = $("input:radio[name=fav-toy]").val();
+    var firstName =$("#first-name").val();
 
+    $(".first-name").text(firstName);
 
-debugger;
     if (design === "likes-design") {
       $("#design-img").show();
-      $(".show-results").show();
+      $(".show-results-design").show();
+      $(".show-results-c-sharp").hide();
+      $(".show-results-ruby").hide();
       $(".hide-results").hide();
       $("#ruby-img").hide();
       $("#c-img").hide();
     } else if (design === "likes-backend" && busType === "start-up" || busType === "med-size" ||  websiteType === "web-apps") {
       $("#ruby-img").show();
-      $(".show-results").show();
+      $(".show-results-ruby").show();
+      $(".show-results-c-sharp").hide();
+      $(".show-results-design").hide();
       $(".hide-results").hide();
       $("#design-img").hide();
       $("#c-img").hide();
     } else if (design === "likes-backend" && busType === "corp" || websiteType === "enterprise") {
       $("#c-img").show();
-      $(".show-results").show();
+      $(".show-results-c-sharp").show();
+      $(".show-results-design").hide();
+      $(".show-results-ruby").hide();
       $(".hide-results").hide();
       $("#ruby-img").hide();
       $("#design-img").hide();
     } else {
       $("#design-img").show();
+      $(".show-results-design").show();
+      $(".show-results-c-sharp").hide();
+      $(".show-results-ruby").hide();
       $("#ruby-img").hide();
       $("#c-img").hide();
     }
