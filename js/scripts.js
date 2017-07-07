984652d447a442f2c0dd215585c1fd64d3f69cdf
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-07 14:40:02 -0700

    add background texture to container, add transparent background to survey portion, create fadeins for programming language images.

--- js/scripts.js
+++ js/scripts.js
@@ -11,32 +11,32 @@ $(function(){
     $(".first-name").text(firstName);
 
     if (design === "likes-design") {
-      $("#design-img").show();
-      $(".show-results-design").show();
-      $(".show-results-c-sharp").hide();  
+      $("#design-img").fadeIn(1200);
+      $(".show-results-design").fadeIn(1200);
+      $(".show-results-c-sharp").hide();
       $(".show-results-ruby").hide();
       $(".hide-results").hide();
       $("#ruby-img").hide();
       $("#c-img").hide();
     } else if (design === "likes-backend" && busType === "start-up" || busType === "med-size" ||  websiteType === "web-apps") {
-      $("#ruby-img").show();
-      $(".show-results-ruby").show();
+      $("#ruby-img").fadeIn(1200);
+      $(".show-results-ruby").fadeIn(1200);
       $(".show-results-c-sharp").hide();
       $(".show-results-design").hide();
       $(".hide-results").hide();
       $("#design-img").hide();
       $("#c-img").hide();
     } else if (design === "likes-backend" && busType === "corp" || websiteType === "enterprise") {
-      $("#c-img").show();
-      $(".show-results-c-sharp").show();
+      $("#c-img").fadeIn(1200);
+      $(".show-results-c-sharp").fadeIn(1200);
       $(".show-results-design").hide();
       $(".show-results-ruby").hide();
       $(".hide-results").hide();
       $("#ruby-img").hide();
       $("#design-img").hide();
     } else {
-      $("#design-img").show();
-      $(".show-results-design").show();
+      $("#design-img").fadeIn(1200);
+      $(".show-results-design").fadeIn(1200);
       $(".show-results-c-sharp").hide();
       $(".show-results-ruby").hide();
       $("#ruby-img").hide();
